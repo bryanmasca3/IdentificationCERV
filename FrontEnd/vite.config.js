6cbc73ca3d656554297fcbf160899cf9c87bb1a9
lan <ffreelance7@gamail.com>
2023-11-02 18:50:46 +0100

    last changes

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "/src/main.jsx",
-    },
-  },
-});
+})
