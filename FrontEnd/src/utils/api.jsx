2c198d9fee0ae1d13fdfb43e6e8ee726c9c56883
lan <ffreelance7@gamail.com>
2023-11-02 18:17:24 +0100

    change api BACKEND

--- FrontEnd/src/utils/api.jsx
+++ FrontEnd/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api/', // Reemplaza 'URL_GENERAL_DE_LA_API' por la URL base de tu API
+  baseURL: 'https://identidicationcerv.onrender.com/api/', // Reemplaza 'URL_GENERAL_DE_LA_API' por la URL base de tu API
 });
 
 export default api;
