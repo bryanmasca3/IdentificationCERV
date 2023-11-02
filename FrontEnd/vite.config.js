669dc1cd52effe12f127e415e3d5eeedd90e7003
lan <ffreelance7@gamail.com>
2023-11-02 18:30:29 +0100

    config V2

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: "/src/main.js",
+      input: "/src/main.jsx",
     },
   },
 });
